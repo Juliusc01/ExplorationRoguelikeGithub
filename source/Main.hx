089a25c470b16d64a0d8d101fec0d4434eda1cb8
Alex Vrhel <avrhel@cs.washington.edu>
2018-04-26 14:29:22 -0700

    changing doors to be 2x1

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,6 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(336, 304, MenuState));
+		addChild(new FlxGame(352, 320, MenuState));
 	}
 }
