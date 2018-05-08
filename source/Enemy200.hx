ae899f628f539970a4045523d07ab532b70a9380
JULIUS DANIEL CHRISTENSON <juliusc@cs.washington.edu>
2018-05-07 20:29:51 -0700

    Adding bunny

--- source/Enemy200.hx
+++ source/Enemy200.hx
@@ -17,16 +17,13 @@ using flixel.util.FlxSpriteUtil;
 class Enemy200 extends Enemy {
     public function new(X:Float = 0, Y:Float = 0, EType:Int) {
 		super(X, Y, EType);
+		loadGraphic("assets/images/Mob/HareA.png", true, 16, 16);
 		setFacingFlip(FlxObject.LEFT, false, false);
         setFacingFlip(FlxObject.RIGHT, true, false);
-        animation.add("d", [0, 1, 0, 2], 6, false);
-        animation.add("lr", [3, 4, 3, 5], 6, false);
-        animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("lr", [0, 1, 2, 3, 4, 5, 6], 6, true);
 		drag.x = drag.y = 10;
-        width = 8;
-        height = 14;
-        offset.x = 4;
-        offset.y = 2;
+        width = 16;
+        height = 16;
 		speed = 80;
 		damage = 0;
 		knockback = 0;
@@ -53,31 +50,15 @@ class Enemy200 extends Enemy {
 	}
 	
 	override public function draw():Void {
-        if ((velocity.x != 0 || velocity.y != 0 ) && touching == FlxObject.NONE) {
-            if (Math.abs(velocity.x) > Math.abs(velocity.y)) {
-                if (velocity.x < 0)
-                    facing = FlxObject.LEFT;
-                else
-                    facing = FlxObject.RIGHT;
-            } else {
-                if (velocity.y < 0)
-                    facing = FlxObject.UP;
-                else
-                    facing = FlxObject.DOWN;
-            }
-
-            switch (facing) {
-                case FlxObject.LEFT, FlxObject.RIGHT:
-                    animation.play("lr");
-
-                case FlxObject.UP:
-                    animation.play("u");
-
-                case FlxObject.DOWN:
-                    animation.play("d");
-            }
-        }
-        super.draw();
+		if (alive) {
+			if(playerPos.x <= this.x) {
+				facing = FlxObject.RIGHT;
+			} else {
+				facing = FlxObject.LEFT;
+			}
+			animation.play("lr");
+			super.draw();
+		}
     }
 
 	override public function update(elapsed:Float):Void {
